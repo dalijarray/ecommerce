6518591d59f019fb7552560b1aa0f3915c6a51c6
AbidellaouiMariem <mariem.abidellaoui@ensit.u-tunis.tn>
2024-04-04 07:07:20 +0200

    mariem first commit

--- backend/src/main/java/com/example/demo/DemoApplication.java
+++ backend/src/main/java/com/example/demo/DemoApplication.java
@@ -1,14 +1,13 @@
-package backend.ecommerce;
-
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EcommerceApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EcommerceApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
